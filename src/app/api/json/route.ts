40db2f0fe248618d8481b2e501fa921eaef362e6
Amadeo Bon <amadeosph@outlook.com>
2024-06-07 17:11:09 -0300

    first commit

---
+++ src/app/api/json/route.ts
@@ -0,0 +1,8 @@
+import { NextRequest } from "next/server";
+
+export const POST = async (req: NextRequest) => {
+    const body = await req.json()
+    // data; format
+     
+  return new Response("OK");
+};
