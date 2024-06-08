c10c9bc393ee34682a1f56e2671a6979d965b66e
Amadeo Bon <amadeosph@outlook.com>
2024-06-08 13:18:31 -0300

    fix type error

--- src/app/api/json/route.ts
+++ src/app/api/json/route.ts
@@ -1,8 +1,90 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import { ZodTypeAny, number, z } from "zod";
 
+const determineSchemaType = (schema: any) => {
+  if (!schema.hasOwnProperty("type")) {
+    if (Array.isArray(schema)) {
+      return "array";
+    } else {
+      return typeof schema;
+    }
+  }
+  return schema.type
+};
+
+const jsonSchemaToZod = (schema: any): ZodTypeAny => {
+  const type = determineSchemaType(schema);
+
+  switch (type) {
+    case "string":
+      return z.string().nullable();
+    case "number":
+      return z.number().nullable();
+    case "boolean":
+      return z.boolean().nullable();
+    case "array":
+      return z.array(jsonSchemaToZod(schema.items)).nullable();
+    case "object":
+      const shape: Record<string, ZodTypeAny> = {};
+
+      // name: {type: string} -------> name: <zodSchemaForString>
+      for (const key in schema) {
+        if (key !== "type") {
+          shape[key] = jsonSchemaToZod(schema[key]);
+        }
+      }
+      return z.object(shape);
+
+    default:
+      throw new Error(`Unsupported data type: ${type}`);
+  }
+};
 export const POST = async (req: NextRequest) => {
-    const body = await req.json()
-    // data; format
-     
-  return new Response("OK");
+  const body = await req.json();
+  // step 1: make sure incoming request is valid
+  const genericSchema = z.object({
+    data: z.string(),
+    format: z.object({}).passthrough(),
+  });
+
+  const { data, format } = genericSchema.parse(body);
+  // step 2: create a schema from the expected user format
+
+  const dynamicSchema = jsonSchemaToZod(format);
+
+  // step 3: retry mechanism
+  type PromiseExecutor<T> = (
+    resolve: (value: T) => void,
+    reject: (reason?: any) => void
+  ) => void;
+  class RetryablePromise<T> extends Promise<T> {
+    static async retry<T>(
+      retries: number,
+      executor: PromiseExecutor<T>
+    ): Promise<T> {
+      return new RetryablePromise(executor).catch((error) => {
+        console.log(`Retrying due to error: ${error}`);
+        return retries > 0
+          ? RetryablePromise.retry(retries - 1, executor)
+          : RetryablePromise.reject(error);
+      });
+    }
+  }
+
+  const validationResult = RetryablePromise.retry<object>(
+    5,
+    (resolve, reject) => {
+      try {
+        //call AI
+        const res = "{name: 'John'}";
+        // validate json
+        const validationResult = dynamicSchema.parse(JSON.parse(res));
+        return resolve(validationResult);
+      } catch (err) {
+        reject(err);
+      }
+    }
+  );
+
+  return NextResponse.json(validationResult, { status: 200 });
 };
